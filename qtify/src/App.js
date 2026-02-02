b4e39c35d117ffca75b5db46115482f950a486c7
Rudransh27 <SSGM_306712@ssgmce.ac.in>
2026-02-02 13:02:12 +0530

    fourth commit --> added the first row of albums

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Card from './components/Card/Card';
+import Section from './components/Section/Section';
 function App() {
   return (
     <div>
       <Navbar />
       <Hero/>
+       <Section title="Top Albums" /> 
     </div>
   );
 }
--- qtify/src/components/Button/Button.jsx
+++ qtify/src/components/Button/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Button.module.css";
-export default function Button({text}){
-    return(
-        <button className={styles.button}>{text}</button>
-    )
-    
+export default function Button({ text, onClick }) {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
