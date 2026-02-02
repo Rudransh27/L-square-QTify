e62011c65b3c17c790d4f98218df613a4e547d36
Rudransh27 <SSGM_306712@ssgmce.ac.in>
2026-02-02 13:14:47 +0530

    change

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Card from './components/Card/Card';
+import Hero from './components/Hero/Hero';  
 import Section from './components/Section/Section';
 function App() {
   return (
