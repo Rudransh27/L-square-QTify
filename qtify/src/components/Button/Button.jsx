900ab32064c4feda1d500fef12021ebd4bb1bbe5
Rudransh27 <SSGM_306712@ssgmce.ac.in>
2026-04-04 11:23:32 +0530

    Test commit

--- qtify/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./Button.module.css";
-export default function Button({ text, onClick }) {
-  return (
-    <button className={styles.button} onClick={onClick}>
-      {text}
-    </button>
-  );
-}
